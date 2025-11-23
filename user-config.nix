f58f87dcc5c0544f0d9ed3ed4f8eaaee9dd7eff1
Satyasheel <satyasheel@lightricks.com>
2025-11-23 23:31:44 +0000

    refactor: Update user configuration, remove applications, and improve Nix installation script robustness.

--- user-config.nix
+++ user-config.nix
@@ -1,8 +1,8 @@
 {
-  username = "satyasheel";
-  fullName = "Satyasheel";
+  username = "satya";
+  fullName = "Satyasheel Satyasheel";
   email = "satyasheel@lightricks.com";
   githubUsername = "mlgruby";
-  hostname = "satyasheel-MBP";
-  signingKey = ""; # Will be set up later if needed
+  hostname = "satya-mbp";
+  signingKey = "A4079973043B6454"; # Will be set up later if needed
 }
